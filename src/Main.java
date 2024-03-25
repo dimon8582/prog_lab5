0af4c7c6f4047d06c0308f99b431a8d8458b6f57
dimon8582 <dimasidim8582@gmail.com>
2024-03-25 21:41:08 +0300

    lab5 2.3 final

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
 import commandManagers.CommandInvoker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         CommandInvoker invoker = CommandInvoker.getInstance();
         invoker.listenCommands();
+
+        // пример для теории
+//        List<Animal> animalList = new ArrayList<>();
+////        animalList.add(new Object());
+//        Cat cat = (Cat) animalList.get(0);
+//        System.out.println(cat.meow());
+//
+//        List<Object> obj = new ArrayList<>();
+//        obj.add(new Animal());
     }
 }
+
+// пример для теории
+//class Animal {
+//    String name;
+//}
+//
+//class Cat extends Animal{
+//    public String meow(){
+//        return "Meow";
+//    }
+//}
